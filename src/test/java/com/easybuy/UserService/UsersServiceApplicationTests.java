bf09dc1060226083df397106f1d8fdc88b948aac
rajeshnadipalli1601 <ben683742@gmail.com>
2026-04-06 12:54:27 +0530

    Adding local profile

--- src/test/java/com/easybuy/UserService/UsersServiceApplicationTests.java
+++ src/test/java/com/easybuy/UserService/UsersServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.easybuy.UserService;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("local")
 class UsersServiceApplicationTests {
 
 	@Test
