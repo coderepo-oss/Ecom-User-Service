1d24b06f3f8cbce7a82048936bac0aec6ec788dd
rajeshnadipalli1601 <ben683742@gmail.com>
2026-05-17 12:56:39 +0530

    US-FS1-Added forgot password

--- src/main/java/com/easybuy/UserService/Controller/UserSignupController.java
+++ src/main/java/com/easybuy/UserService/Controller/UserSignupController.java
@@ -1,18 +1,19 @@
 package com.easybuy.UserService.Controller;
 
+import com.easybuy.UserService.DTO.ForgotPasswordRequest;
 import com.easybuy.UserService.Entity.UserSignup;
 import com.easybuy.UserService.Service.UserService;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/users")
-public class UserSignupController
-{
+public class UserSignupController {
+
     private final UserService userService;
 
     public UserSignupController(UserService userService) {
@@ -20,9 +21,16 @@ public class UserSignupController
     }
 
     @PostMapping("/signup")
-    public ResponseEntity<UserSignup> signup(@RequestBody UserSignup user)
-    {
+    public ResponseEntity<UserSignup> signup(@RequestBody UserSignup user) {
+        log.info("Signup request received");
         UserSignup savedUser = userService.register(user);
         return ResponseEntity.status(HttpStatus.CREATED).body(savedUser);
+    }
+
+    @PostMapping("/forgot-password")
+    public ResponseEntity<String> forgotPassword(@RequestBody @Valid ForgotPasswordRequest request) {
+        log.info("Forgot password request received");
+        userService.forgotPassword(request.getEmail());
+        return ResponseEntity.ok("Temporary password sent to your email");
     }
 }
